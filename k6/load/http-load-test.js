f4f75805f941f6000998a75bca308be827a89294
Shain Singh <shain.singh@gmail.com>
2023-03-20 22:40:28 +1100

    update k6 files

--- k6/load/http-load-test.js
+++ k6/load/http-load-test.js
@@ -1,15 +1,15 @@
-import http from 'k6/http'
-import { check } from 'k6'
+import http from "k6/http";
+import { check } from "k6";
 
 // See https://k6.io/docs/using-k6/options
 export const options = {
   stages: [
-    { duration: '15s', target: 2000 },
-    { duration: '1m', target: 2000 },
-    { duration: '15s', target: 0 },
+    { duration: "15s", target: 2000 },
+    { duration: "1m", target: 2000 },
+    { duration: "15s", target: 0 },
   ],
-}
+};
 
 export default function main() {
-  let response = http.get('https://bank.f5labs.dev/')
+  let response = http.get("https://bank.f5labs.dev/");
 }
