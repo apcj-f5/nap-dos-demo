1b8d45af0c2e1a77cd74905ce6095343eef68933
Shain Singh <s.singh@f5.com>
2023-07-11 10:06:14 +1000

    update http baseline test

--- k6/baseline/http-baseline-test.js
+++ k6/baseline/http-baseline-test.js
@@ -5,9 +5,8 @@ import { check } from "k6";
 export const options = {
   stages: [{ duration: "1m", target: 1 }],
   thresholds: {
-    checks: ["rate<0.02"], // http errors should be less than 2%
     http_req_failed: ["rate<0.02"], // http errors should be less than 2%
-    http_req_duration: ["p(95)<2000"], // 95% requests should be below 2s
+    http_req_duration: ["p(90)<2000", "p(95)<2500"], // 90% of requests should be below 2s, 95% should be below 2.5s
   },
 };
 
@@ -16,10 +15,12 @@ const USERNAME = "alice";
 const PASSWORD = "bankofanthos";
 
 export default function main() {
-  const res = http.post(`${BASE_URL}/login/`, {
+  const postResponse = http.post(`${BASE_URL}/login`, {
     username: USERNAME,
     password: PASSWORD,
   });
 
-  check(res, { "status is 200": (r) => r && r.status === 200 });
+  const redirectResponse = http.get(`${BASE_URL}/home`);
+
+  check(redirectResponse, { "status is 200": (r) => r && r.status === 200 });
 }
